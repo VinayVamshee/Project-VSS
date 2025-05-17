062e1f336ea9d1c96e2ba88bde3a10aafb94ab17
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-05-17 14:16:13 +0530

    Update Navigation.js

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Navigation() {
+  const location = useLocation();
+
   const [theme, setTheme] = useState(() => {
-    // Check localStorage for saved theme or default to 'white'
     return localStorage.getItem('theme') || 'white';
   });
 
@@ -12,10 +13,19 @@ export default function Navigation() {
     localStorage.setItem('theme', theme);
   }, [theme]);
 
+  const isActive = (path) => location.pathname === path ? 'active' : '';
+
   return (
     <div className='Navigation'>
-      <Link className='btn' to='/new-sheet'>Register New Case</Link>
-      <Link className='btn' to='/'>Cases</Link>
+      <Link className={`btn ${isActive('/new-sheet')}`} to='/new-sheet'>
+        <i className="fa-solid fa-square-plus fa-lg me-2"></i>Register New Case
+      </Link>
+      <Link className={`btn ${isActive('/inProgress')}`} to='/inProgress'>
+        <i className="fa-solid fa-circle-dot fa-lg me-1" style={{ color: '#ffc800' }}></i>In Progress
+      </Link>
+      <Link className={`btn ${isActive('/closedCases')}`} to='/closedCases'>
+        <i className="fa-solid fa-circle-dot fa-lg me-1" style={{ color: 'red' }}></i>Closed Cases
+      </Link>
       <select value={theme} onChange={(e) => setTheme(e.target.value)} className='btn'>
         <option value="white">☀️ Light</option>
         <option value="black">🌑 Dark</option>
